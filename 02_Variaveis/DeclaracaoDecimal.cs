a2195afd03392971f5e90b2b49953d0e4f1865c9
Hanna Amaral <hannaap0001@gmail.com>
2025-08-01 22:02:41 -0300

    VariaveisDecimal

---
+++ 02_Variaveis/DeclaracaoDecimal.cs
@@ -0,0 +1,12 @@
+using System;
+
+class DeclaracaoVariaveisDecimal
+	{
+		static void Main(string[] args)
+		{
+			//decimal myMoneyError = 300.0;
+			decimal myMoneyCorrect = 300.00m;
+			Console.WriteLine(myMoneyCorrect);
+			
+		}
+	}
---
+++ 02_Variaveis/DeclaracaoVariaveis.cs
@@ -0,0 +1,21 @@
+using System;
+
+class DeclaracaoVariaveis
+{
+	static void Main()
+	{
+		int a;
+		a = 22;
+		Console.WriteLine(a);
+		
+		//Reatribuação de valor na Váriavel
+		a = 56;
+		Console.WriteLine;
+		//Outras formas de declarar uma Váriavel
+		
+		//Declaração e inicialização da Váriavel
+		
+	int b = 925;
+	
+	}
+}
