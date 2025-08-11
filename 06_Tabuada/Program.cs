22954c7f21ff231d71eb4cfaf95663582a30a96f
Hanna Amaral <hannaap0001@gmail.com>
2025-08-11 20:14:50 -0300

    atribuição da atividade em C# 06_tabuada

---
+++ 06_Tabuada/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace _06_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int multiplicando = 0, produto;
+            bool digitacaoValida = false;
+
+            // ! negação: inverte p vaçpr bppçeamp
+            // !false = true
+            // !true = false
+            // while (digitaçãoValida == false)
+            while(!digitacaoValida)
+            {
+                Console.WriteLine("Digite um número inteiro");
+                digitacaoValida = int.TryParse(Console.ReadLine(), out multiplicando);
+            }
+            Console.WriteLine($"TABUADA DO {multiplicando}");
+            for (int multiplicador = 1; multiplicador <= 10; multiplicador++)
+            {
+                produto = multiplicando * multiplicador;
+                Console.WriteLine($"{multiplicando} x {multiplicador} = {produto}");
+            }
+        }
+    }
+}
