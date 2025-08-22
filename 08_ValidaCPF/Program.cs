1c369193ac1c5cd17aa283dfbc08d8b33f403ddf
Hanna Amaral <hannaap0001@gmail.com>
2025-08-22 20:24:46 -0300

    Criação do método CalculaDV

--- 08_ValidaCPF/Program.cs
+++ 08_ValidaCPF/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace _08_ValidaCPF
 {
@@ -11,65 +8,69 @@ namespace _08_ValidaCPF
     {
         static void Main(string[] args)
         {
-            //pedi cpf e deixei só em numero
-            Console.WriteLine("Digite o Cpf:");
+
+            Console.Write("Digite o CPF: ");
             string cpf = Console.ReadLine();
-            cpf =Regex.Replace(cpf, "[^0-9]", "");
-            
-            //validei pra saber se tinha 11 numeros msm
+
+            // 1 - Eliminar caractres não numéricos
+            //cpf.replace("'.", "");
+            //cpf.Replace("'-", "");
+            cpf = Regex.Replace(cpf, "[^0-9]", "");
+
+            // 2 - Validar se tem 11 digitos
             if (cpf.Length != 11)
             {
-                Console.WriteLine("Cpf deve conter 11 digitos.");
+                Console.WriteLine("CPF deve conter 11 digitos");
                 return;
             }
 
-            //sem repetição
-            if (cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" ||
-                cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" ||
-                cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999"
+            // 3- Validas CPFs com todos os números iguais
+            /* if( cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" ||
+                 cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" ||
+                 cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999"                 
+             )*/
+            if (cpf.Distinct().Count() == 1)
+            {
+                Console.WriteLine("CPF inválido! Números repetidos não são permitidos");
+                return;
+            }
+
+           
+            int digX = CalculaDV(cpf, 9, 10);
+            int digY = CalculaDV(cpf, 10, 11);
+
+            //6 - Comparar os dígitos
+            if (
+                int.Parse(cpf[9].ToString()) == digX &&
+                int.Parse(cpf[10].ToString()) == digY
                )
             {
-                Console.WriteLine("Cpf inválido! Números repetidos não são permitidos");
-                return;
-            }
-
-
-            int validacao = 10;
-            int soma = 0;
-            int d10 = 0;
-
-            //calculando o digito 1 (primeiro)
-            for (int i = 0; i < 9; i++)
-            {
-                int resultado = (cpf[i] - '0') * validacao;
-                validacao--;
-                soma += resultado;
-            }
-
-            d10 = soma % 11;
-            d10 = (d10 <= 2) ? 0 : 11 - d10;
-            
-            soma = 0;
-            int d11 = 0;
-            int validacao1 = 11;
-            for (int i = 0; i < 10; i++)
-            {
-                int resultado = (cpf[i] - '0') * validacao1;
-                validacao1--;
-                soma += resultado;
-            }
-
-            d11 = soma % 11;
-            d11 = (d11 <= 2) ? 0 : 11 - d11;
-
-            if ((cpf[9] - '0') == d10 && (cpf[10] - '0') == d11)
-            {
-                Console.WriteLine("CPF válido.");
+                Console.WriteLine("CPF VÁLIDO!");
             }
             else
             {
-                Console.WriteLine("CPF inválido.");
+                Console.WriteLine("CPF INVÁLIDO!");
             }
         }
+
+        public static int CalculaDV(string cpf, int qtdeNumeros, int peso)
+        {
+            int soma = 0;
+            char[] cpfVetor = cpf.ToCharArray();
+
+            for (int i = 0; i < qtdeNumeros; i++)
+            {
+                soma += int.Parse(cpfVetor[i].ToString()) * (peso - i);
+            }
+            int resto = soma % 11;
+
+            int digito = 0;
+            if (resto >= 2)
+            {
+                digito = 11 - resto;
+            }
+            return digito;
+        }
+
     }
 }
